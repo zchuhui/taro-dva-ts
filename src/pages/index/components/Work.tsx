a570a7402e079e8fc5f5d6583e6ca478d173dfd2
zchuhui <943339564@qq.com>
2019-12-18 11:20:30 +0800

    授权

--- src/pages/index/components/Work.tsx
+++ src/pages/index/components/Work.tsx
@@ -1,5 +1,5 @@
 import Taro, { Component } from '@tarojs/taro';
-import { View, Button, CoverImage, WebView } from '@tarojs/components';
+import { View, Button, CoverImage } from '@tarojs/components';
 import './Work.scss';
 import EditIcon from '../../../static/images/icon-edit.png';
 
@@ -17,38 +17,44 @@ export default class Work extends Component<Props, State> {
         <View className="work-content">
           <Button
             className="btn-setup"
-            onClick={() => {
-              Taro.navigateTo({ url: 'www.baidu.com' });
-            }}
+            // onClick={() => {
+            //   Taro.navigateTo({ url: 'www.baidu.com' });
+            // }}
           >
-            设置您的运动数据来源
+            设置您的运动数据来源 <View className="icon-add"></View>
           </Button>
 
           <View className="act-types">
-            <View className="item">
-              <View className="i-type"> 跑步 13km</View>
+            <View className="item item-run">
+              <View className="i-type">
+                {' '}
+                跑步 13 <View className="km">km</View>{' '}
+              </View>
               <View className="i-address"> 美国华盛顿州 · xxxx大峡谷 </View>
               <View className="i-btn">
-                {' '}
-                <CoverImage src={EditIcon} />{' '}
+                <CoverImage className="icon-img" src={EditIcon} />
               </View>
             </View>
 
-            <View className="item">
-              <View className="i-type"> 跑步 13km</View>
+            <View className="item item-bike">
+              <View className="i-type">
+                {' '}
+                骑行 13 <View className="km">km</View>
+              </View>
               <View className="i-address"> 美国华盛顿州 · xxxx大峡谷 </View>
               <View className="i-btn">
-                {' '}
-                <CoverImage src={EditIcon} />{' '}
+                <CoverImage className="icon-img" src={EditIcon} />
               </View>
             </View>
 
-            <View className="item">
-              <View className="i-type"> 跑步 13km</View>
+            <View className="item item-onfoot">
+              <View className="i-type">
+                {' '}
+                徒步 13 <View className="km">km</View>
+              </View>
               <View className="i-address"> 美国华盛顿州 · xxxx大峡谷 </View>
               <View className="i-btn">
-                {' '}
-                <CoverImage src={EditIcon} />{' '}
+                <CoverImage className="icon-img" src={EditIcon} />
               </View>
             </View>
           </View>
