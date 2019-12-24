96654cd7faef6f27918d00980eb94cdccdd5da60
zchuhui <943339564@qq.com>
2019-12-24 15:26:40 +0800

    调整导航菜单

--- src/pages/index/components/TabBar.tsx
+++ src/pages/index/components/TabBar.tsx
@@ -30,18 +30,21 @@ export default class TabBar extends Component<Props, State> {
         <View className="tabbar-item" onClick={this.toPage.bind(this, 0)}>
           <View className={`tabbar-box ${current === 0 || current === undefined ? 'tabbar-active' : null}`}>
             <View className="iconfont home"></View>
+            <View>首页</View>
           </View>
         </View>
 
         <View className="tabbar-item" onClick={this.toPage.bind(this, 1)}>
           <View className={`tabbar-box ${current === 1 ? 'tabbar-active' : null}`}>
             <View className="iconfont work"></View>
+            <View>制作</View>
           </View>
         </View>
 
         <View className="tabbar-item" onClick={this.toPage.bind(this, 2)}>
           <View className={`tabbar-box ${current === 2 ? 'tabbar-active' : null}`}>
             <View className="iconfont me"></View>
+            <View>我的</View>
           </View>
         </View>
       </View>
